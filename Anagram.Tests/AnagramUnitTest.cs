53fe8e8bd461660c15ad7ba9bfffe620ccff1d67
Twinkal Parmar <66124568+TwinkalParmar-16@users.noreply.github.com>
2020-09-06 17:49:00 +0530

    Add files via upload

--- Anagram.Tests/AnagramUnitTest.cs
+++ Anagram.Tests/AnagramUnitTest.cs
@@ -9,27 +9,26 @@ namespace Anagram.Tests
     public class AnagramUnitTest
     {
         [Fact]
-        public void RecognizesShuffledAlphabetsAsAnagrams()
+        public void ReturnTrueForGivenAnagramPair()
         {
             AnagramSelector selector = new AnagramSelector();
-            Assert.True(selector.WordPairIsAnagram("restful", "fluster"));
-            Assert.True(selector.WordPairIsAnagram("forty five", "over fifty"));
+            Assert.True(selector.GivenStringPairThenCheckIsAnagram("restful", "fluster"));
+            Assert.True(selector.GivenStringPairThenCheckIsAnagram("forty five", "over fifty"));
+            Assert.True(selector.GivenStringPairThenCheckIsAnagram("",""));
         }
         [Fact]
-        public void ReportsNonAnagrams()
+        public void ReturnFalseForGivenAnag()
         {
             AnagramSelector selector = new AnagramSelector();
-            Assert.False(selector.WordPairIsAnagram("something", "else"));
+            Assert.False(selector.GivenStringPairThenCheckIsAnagram("something", "else"));
+            Assert.False(selector.GivenStringPairThenCheckIsAnagram("","iAmNotEmpty"));
         }
         [Fact]
-        public void SelectsAnagramsOfAWord()
+        public void TestForSelectsAnagramsOfAWordIsCorrectOrNot()
         {
             AnagramSelector selector = new AnagramSelector();
-            var selection = selector.SelectAnagrams("master",
-                new List<string>{"stream", "something", "maters"});
-
-            Assert.True(selection.SequenceEqual(
-                new List<string>{"stream", "maters"}));
+            var selection = selector.GivenListStringThenSelectAnagrams("master",new List<string>{"stream", "something", "maters"});
+            Assert.True(selection.SequenceEqual(new List<string>{"stream", "maters"}));
         }
     }
 }
